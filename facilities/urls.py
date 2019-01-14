ec51f0b3a193beeb9d9a4f1e2ba1f294a2eb5547
Drew Cannedy <dxc142830@utdallas.edu>
2019-01-13 22:17:07 -0600

    created nav, create facilities app

--- facilities/urls.py
+++ facilities/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.urls import path
 
+from facilities import views
+
+app_name = 'facilities'
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- facilities/views.py
+++ facilities/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'facilities/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['name'] = user.first_name + " " + user.last_name 
+        return context
